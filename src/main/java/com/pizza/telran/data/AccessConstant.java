f45c50b983798440eca169cfe7af35a6aad3fd9f
ArturVoldaev <79632566+ArturVoldaev@users.noreply.github.com>
2024-01-04 11:31:02 -0500

    Delete src directory

--- src/main/java/com/pizza/telran/data/AccessConstant.java
+++
@@ -1,8 +0,0 @@
-package com.pizza.telran.data;
-
-public class AccessConstant {
-    public static final String BASE_USER =  "user";
-    public static final String BASE_ADMIN =  "admin";
-    public static final String BASE_PASSWORD =  "1";
-    public static final String RANDOM_USER =  new GenerateRandomData().generateFirstName();
-}
